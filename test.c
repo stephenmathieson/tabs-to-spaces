fa6ec7519e2f3c4a7ea5054352a336569ec07608
Stephen Mathieson <smath23@gmail.com>
2014-02-26 14:06:47 -0500

    Refactor tests

--- test.c
+++ test.c
@@ -1,14 +1,28 @@
 
+#include <stdlib.h>
 #include <string.h>
-#include <assert.h>
+#include <stdio.h>
 #include "src/tabs-to-spaces.h"
 
-int main(int argc, char **argv) {
-  assert(0 == strcmp("hello world", tabs_to_spaces("hello\tworld", 1)));
-  assert(0 == strcmp("hello  world", tabs_to_spaces("hello\tworld", 2)));
-  assert(0 == strcmp("  hello world", tabs_to_spaces("\thello world", 2)));
-  assert(0 == strcmp("  hello  world  ", tabs_to_spaces("\thello\tworld\t", 2)));
-  assert(0 == strcmp("hello world  ", tabs_to_spaces("hello world\t", 2)));
-  assert(0 == strcmp("          ", tabs_to_spaces("\t", 10)));
+#define equal(a, b) ({                                \
+  if (0 != strcmp(a, b)) {                            \
+    fprintf(stderr                                    \
+      , "Assertion error: \"%s\" == \"%s\" (%s:%d)\n" \
+      , a                                             \
+      , b                                             \
+      , __FILE__                                      \
+      , __LINE__);                                    \
+    exit(1);                                          \
+  }                                                   \
+});
+
+int
+main(void) {
+  equal("hello world",      tabs_to_spaces("hello\tworld", 1));
+  equal("hello  world",     tabs_to_spaces("hello\tworld", 2));
+  equal("  hello world",    tabs_to_spaces("\thello world", 2));
+  equal("  hello  world  ", tabs_to_spaces("\thello\tworld\t", 2));
+  equal("hello world  ",    tabs_to_spaces("hello world\t", 2));
+  equal("          ",       tabs_to_spaces("\t", 10));
   return 0;
 }
