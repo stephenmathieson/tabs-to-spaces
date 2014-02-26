f38152c3add8c3a318466f53c1b7acad632b6e1b
Stephen Mathieson <smath23@gmail.com>
2014-02-26 14:28:49 -0500

    Release 1.0.0

--- src/main.c
+++ src/main.c
@@ -94,7 +94,7 @@ main(int argc, char **argv){
   opts.spaces = 2;
 
   command_t cmd;
-  command_init(&cmd, argv[0], "0.0.1");
+  command_init(&cmd, argv[0], "1.0.0");
   command_option(&cmd
     , "-v"
     , "--verbose"
